ba72b11b3391c59bf3865cb6307adebcd08647e5
Vinnie-10 <strawberry2peach@gmail.com>
2025-05-16 21:35:59 +0700

    Add files via upload

---
+++ HCI/js/scriptrating.js
@@ -0,0 +1,29 @@
+let pfpMenu = document.getElementById("pfpMenu");
+
+function toggleMenu(){
+    pfpMenu.classList.toggle("open-menu");
+}
+
+let dropNotif = document.getElementById("notifDrop");
+
+function toggleNotif(){
+    dropNotif.classList.toggle("open-notif");
+}
+
+const allstars = document.querySelectorAll('.Rcontainer');
+
+allstars.forEach(container => {
+    const stars = container.querySelectorAll('.star');
+
+    stars.forEach((star, index) => {
+        star.addEventListener('click', () => {
+            stars.forEach((s, i) => {
+                if (i <= index){
+                    s.textContent = '★';
+                } else {
+                    s.textContent = '☆'; 
+                }
+            })
+        })
+    })
+})
---
+++ HCI/js/src.js
@@ -0,0 +1,11 @@
+let pfpMenu = document.getElementById("pfpMenu");
+
+function toggleMenu(){
+    pfpMenu.classList.toggle("open-menu");
+}
+
+let dropNotif = document.getElementById("notifDrop");
+
+function toggleNotif(){
+    dropNotif.classList.toggle("open-notif");
+}
